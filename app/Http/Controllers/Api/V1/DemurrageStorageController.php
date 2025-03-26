d57c6c71f807c89d4befb3e5f7ace34406502e45
Laia Fuster <laia.fuster@dachser.com>
2025-03-26 09:46:57 +0100

    :sparkles: añadido calcRes() a api : sparkles:

--- app/Http/Controllers/Api/V1/DemurrageStorageController.php
+++ app/Http/Controllers/Api/V1/DemurrageStorageController.php
@@ -16,27 +16,74 @@ class DemurrageStorageController extends Controller
         //
     }
 
-    public function indexCarrier(){
+    public function indexCarrier()
+    {
 
-       /* dd(DemurrageStorage::select('carrier')->distinct()->get()); */
+        /* dd(DemurrageStorage::select('carrier')->distinct()->get()); */
         return response()->json(DemurrageStorage::select('carrier')->distinct()->get());
     }
 
-    public function indexPorts(){
+    public function indexPorts()
+    {
         return response()->json(DemurrageStorage::select('port')->distinct()->get());
     }
 
 
 
-    public function portsWhereCarrier($carrier){
+    public function portsWhereCarrier($carrier)
+    {
 
         return response()->json(DemurrageStorage::select('port')->where('carrier', $carrier)->distinct()->get());
     }
 
-    public function carriersWherePort($port){
+    public function carriersWherePort($port)
+    {
         return response()->json(DemurrageStorage::select('carrier')->where('port', $port)->distinct()->get());
     }
 
+    public function calcRes(Request $request)
+    {
+        $vessel_arrival = request('vessel_arrival');
+        $gate_out_full = request('gate_out');
+        $gate_empty = request('gate_empty');
+
+        $carrier = request('carrier');
+        $port = request('port');
+        $fromday = 0;
+        $today = 0;
+
+        $type = request('type');
+        $freeSto = request('freeSto');
+        $freeDem = request('freeDem');
+
+        if (request('tar20')) {
+            $tar = request('tar20');
+        } else {
+            $tar = request('tar40');
+        }
+
+        $tarsup = request('tarsup');
+
+        $today = $vessel_arrival - $gate_out_full;
+
+        $storage = DemurrageStorage::where('carrier', $carrier)
+            ->where('port', $port)
+            ->where('fromday', '<=', 0)
+            ->where('today', '>=', $today)
+            ->where('type', 'STO')
+            ->get();
+
+        $demurrage = DemurrageStorage::where('carrier', $carrier)
+            ->where('port', $port)
+            ->where('fromday', '<=', $fromday)
+            ->where('today', '>=', $today)
+            ->where('type', 'DEM')
+            ->get();
+
+
+
+        return response()->json($storage, $demurrage);
+    }
 
     /**
      * Store a newly created resource in storage.
